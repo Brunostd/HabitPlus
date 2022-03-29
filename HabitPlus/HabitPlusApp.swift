480ac28609389534dfcda668ed58d9676557fe55
bruno.std <batioe.std@gmail.com>
2022-03-29 15:34:13 -0300

    Initial Commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Bruno Brito on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
